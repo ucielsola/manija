77b1d2e1ab270108fd45caa9e29e2ad44ee5e70e
Uciel Sola <solauciel@gmail.com>
2024-07-23 00:04:42 -0300

    first try

---
+++ src/lib/components/ui/button/iframe.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	let { url }: { url: string } = $props();
+
+	$effect(() => {
+		console.log(url);
+	});
+</script>
+
+{#if url}
+	<iframe
+		width="560"
+		height="315"
+		src={url}
+		title="YouTube video player"
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+		referrerpolicy="strict-origin-when-cross-origin"
+		allowfullscreen
+	></iframe>
+{/if}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Button from "$lib/components/ui/button/button.svelte";
+	import Iframe from "$lib/components/ui/button/iframe.svelte";
+</script>
+<div class="h-screen w-screen bg-primary">
+<Button variant="outline">Click me</Button>
+
+<Iframe 
+    url="https://www.youtube.com/embed/T3DZTq8T_n0?si=KsdQRMc3H3LpAflV"
+/>
+
+</div>
+
+
