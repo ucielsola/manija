26e37a72b0816520faab08bc6c935f240e87cc05
Uciel Sola <solauciel@gmail.com>
2025-03-12 13:01:38 -0300

    add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.css';
 
 	import { Toaster } from '$lib/components/ui/sonner';
+
+	import { inject } from '@vercel/analytics';
+ 
+	inject();
 </script>
 
 <div class="bg-primary h-screen w-screen overflow-x-hidden overflow-y-hidden">
