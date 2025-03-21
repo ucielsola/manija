dab9a8dc2f699f188a6cdd94f87756c1d00a18e6
Uciel Sola <solauciel@gmail.com>
2025-03-20 22:47:51 -0300

    remove all

--- src/lib/stores/index.ts
+++
@@ -1,7 +0,0 @@
-import { SourceList } from "$lib/models/sourceList.svelte";
-import { ToastStore } from "$lib/models/toasts.svelte";
-import { App } from "$lib/models/app.svelte";
-
-export const app = new App();
-export const sourceList = new SourceList();
-export const toastStore = new ToastStore();
--- src/lib/types/Toast.ts
+++
@@ -1,7 +0,0 @@
-import type { ColorType } from "./common";
-
-export interface Toast {
-    id: string;
-    text: string;
-    type: ColorType;
-}
