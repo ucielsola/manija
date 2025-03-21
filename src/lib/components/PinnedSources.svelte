40e1a8f918894d89e05cf85b2c25bc64a16508cb
Uciel Sola <solauciel@gmail.com>
2025-03-20 23:00:55 -0300

    fix layout

--- src/lib/components/PinnedSources.svelte
+++ src/lib/components/PinnedSources.svelte
@@ -8,19 +8,19 @@
 	let pinnedSources = $derived(sourceList.pinned);
 </script>
 
-<div class="flex flex-col h-full w-full overflow-hidden">
-	<div class="p-8 pt-0 h-full overflow-hidden">
-		<div
-			class="grid h-full align-top gap-4 overflow-scroll py-8 auto-rows-min"
-			style={`grid-template-columns: repeat(${columns}, 1fr);`}
-		>
-			{#each pinnedSources as source (source.id)}
-				<NewSource {source} />
-			{/each}
-		</div>
-	</div>
-
+<div class="flex h-full w-full flex-col overflow-hidden">
 	{#if pinnedSources.length === 0}
 		<Guide />
+	{:else}
+		<div class="h-full overflow-hidden p-8 pt-0">
+			<div
+				class="grid h-full auto-rows-min gap-4 overflow-scroll py-8 align-top"
+				style={`grid-template-columns: repeat(${columns}, 1fr);`}
+			>
+				{#each pinnedSources as source (source.id)}
+					<NewSource {source} />
+				{/each}
+			</div>
+		</div>
 	{/if}
 </div>
