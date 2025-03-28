1655b1983816597c8ca05717a708023e563a65d9
Uciel Sola <solauciel@gmail.com>
2025-03-28 17:58:52 -0300

    feat: add video button on pinned sources

--- apps/frontend/src/lib/components/PinnedSources.svelte
+++ apps/frontend/src/lib/components/PinnedSources.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import { Plus } from '$lib/components/icons';
+	
 	import Source from '$lib/components/Source.svelte';
 	import Guide from '$lib/components/Guide.svelte';
 
 	import { app, sourceList } from '$lib/stores';
-
+	
 	let columns = $derived(app.columns);
 	let pinnedSources = $derived(sourceList.pinned);
 </script>
@@ -20,6 +22,21 @@
 				{#each pinnedSources as source (source.id)}
 					<Source {source} />
 				{/each}
+
+				<div class="flex min-h-48 gap-4">
+					<div
+						class="flex h-full w-full items-center justify-center gap-4 rounded-md border border-primary border-dashed px-8 py-6"
+					>
+						<button
+							class="btn btn-outline btn-wide btn-primary"
+							onclick={() => (app.showAddSource = true)}
+							aria-label="Add Video"
+						>
+							<Plus className="w-4 h-4" />
+							Agregar Video
+						</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	{/if}
