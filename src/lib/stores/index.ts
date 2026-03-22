4aae5075fa08db9451d5c391125f96846707e227
Uciel Sola <solauciel@gmail.com>
2026-03-22 00:26:06 -0300

    refactor: update store exports to use new model names

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,9 +1,9 @@
-import { SourceList } from '$lib/models/sourceList.svelte';
-import { ApiSourceList } from '$lib/models/apiSourceList.svelte';
+import { UserSources } from '$lib/models/userSources.svelte';
+import { ManijaSources } from '$lib/models/manijaSources.svelte';
 import { ToastStore } from '$lib/models/toasts.svelte';
 import { App } from '$lib/models/app.svelte';
 
 export const app = new App();
-export const sourceList = new SourceList();
-export const apiSourceList = new ApiSourceList();
+export const userSources = new UserSources();
+export const manijaSources = new ManijaSources();
 export const toastStore = new ToastStore();
