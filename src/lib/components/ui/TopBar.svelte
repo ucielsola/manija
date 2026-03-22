cdff4108873fe365d58de57b10d8f8b765226cf6
Uciel Sola <solauciel@gmail.com>
2026-03-22 00:32:00 -0300

    style: icon in topbar

--- src/lib/components/ui/TopBar.svelte
+++ src/lib/components/ui/TopBar.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <header class="glass fixed top-0 z-50 flex h-14 w-full items-center justify-between px-6">
-	<div class="text-primary flex items-center justify-center gap-3">
+	<div class="text-primary flex items-center gap-3">
 		<Tv />
-		<span class=" font-headline text-xl font-black tracking-tighter">Manija TV</span>
+		<span class=" font-headline pt-1 text-xl font-black">Manija TV</span>
 	</div>
 </header>
